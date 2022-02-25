b7990fdaf2bcba8d071e0cdf69aa70a55ccefda9
fernando3600 <fantoniogonzalez1999@gmail.com>
2022-02-25 12:03:12 -0600

    logo y login terminados

--- app/src/main/java/com/example/evaluacion_practica_1/MainActivity.java
+++ app/src/main/java/com/example/evaluacion_practica_1/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.evaluacion_practica_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnIngresar);
+        Button btnsalir = findViewById(R.id.btnCancelar);
+
+        EditText user = findViewById(R.id.EdTUsuario);
+        EditText pass = findViewById(R.id.EdTPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if( user.getText().toString().equals("DM1p22A")){
+                    Toast.makeText(MainActivity.this, "El usuario es correcto", Toast.LENGTH_SHORT).show();
+                }else{
+                    showAlertDialog();
+                }
+
+            }
+        });
+
+        btnsalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void showAlertDialog(){
+        new AlertDialog.Builder(this)
+                .setTitle("Error de Autentificacion: ")
+                .setMessage("el Usuario es incorrecto")
+                .setPositiveButton("aceptar", null).show();
     }
 }
